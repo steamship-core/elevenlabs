b915cccc94668ce6633b4b97e1455a06cbbc79a4
Ted Benson <ted@steamship.com>
2023-09-12 18:46:21 -0400

    Add usage reporting

--- src/api.py
+++ src/api.py
@@ -216,6 +216,7 @@ class ElevenlabsPlugin(StreamingGenerator):
         ]
         return InvocableResponse(
             data=StreamCompletePluginOutput(
+                usage=usage
             ),
         )
 
